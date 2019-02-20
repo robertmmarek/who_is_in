1a577e9451b42a058bbe147c03075bb9f2009439
AIRCOMAI\r.marek <rmarek@aircom.ag>
2019-02-20 14:24:17 +0100

    navbar added

--- who_is_in/who_is_in/views.py
+++ who_is_in/who_is_in/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'main_template.html')
+    return render(request, 'main_template.html', context={'path_name':'index'})
