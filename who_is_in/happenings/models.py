c7e744ba46eb57bf37c60a7b7aae368b61f8acb0
AIRCOMAI\r.marek <rmarek@aircom.ag>
2019-02-22 14:48:00 +0100

    profile is created for non-existing user when there is no profile for logged user

--- who_is_in/happenings/models.py
+++ who_is_in/happenings/models.py
@@ -3,21 +3,21 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Hashtag(models.Model):
+    hashtag_name = models.CharField(max_length=128)
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     description = models.CharField(max_length=2048)
     observed_hashtags = models.ManyToManyField(Hashtag)
-
-class Hashtag(models.Model):
-    hashtag_name = models.CharField(max_length=128)
 
 class Happening(models.Model):
     creator = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='created_happenings')
     max_participants = models.IntegerField()
     participants = models.ManyToManyField(Profile)
     hashtags = models.ManyToManyField(Hashtag)
-    date = models.DateTimeField()
-    description = models.CharField(max_length=2048)
+    date = models.DateTimeField(auto_now=True)
+    description = models.CharField(max_length=2048, default='')
 
     def add_user(self, user):
         self.participants.add(user)
--- who_is_in/happenings/views.py
+++ who_is_in/happenings/views.py
@@ -1,31 +1,60 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from .models import Profile, Hashtag, Happening
 
 # Create your views here.
 
 def profile(request):
+    handle_profile(request)
     return HttpResponse("profile")
 
 def board(request):
+    handle_profile(request)
     return HttpResponse("board")
 
 def sign_to_happening(request, happening_id):
+    handle_profile(request)
     return HttpResponse("sign_to_happening")
 
 def unsign_from_happening(request, happening_id):
+    handle_profile(request)
     return HttpResponse("unsign_from_happening")
 
 def follow_hashtag(request, hashtag_name):
+    handle_profile(request)
     return HttpResponse("follow_hashtag")
 
 def unfollow_hashtag(request, hashtag_name):
+    handle_profile(request)
     return HttpResponse("unfollow_hashtag")
 
 def happening_create(request, happening_id):
+    handle_profile(request)
     return HttpResponse("happening_create")
 
 def happening_detail(request, happening_id):
+    handle_profile(request)
     return HttpResponse("happening_detail")
 
 def delete_happening(request, happening_id):
+    handle_profile(request)
     return HttpResponse("delete_happening")
+
+# check if profile for logged user exists - if not - create new profile
+# and tie it to profile from request
+def handle_profile(request):
+    if request.user.is_authenticated:
+        reqUser = User.objects.get(username=request.user.username)
+        resultProfile = None
+
+        try:
+            resultProfile = Profile.objects.get(user=reqUser)
+        except Exception as e:
+            resultProfile = None
+
+        if resultProfile == None:
+            tempProfile = Profile()
+            tempProfile.user = reqUser
+            tempProfile.save()
+            reqUser.save()
